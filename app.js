ecf02dc42d8c0222d919185bd9a7439232f04f1f
Magnus <magnuspetersonmunoz@gmail.com>
2019-10-30 02:29:53 +0000

    Signed-off-by: Magnus <magnuspetersonmunoz@gmail.com>
    add header/footer partials

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express"),
 app = express()
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){
+	res.render("index")
+})
 
 app.listen(5004, function(){
-	console.log("App on PORT 5004")
+console.log("App on PORT 5004")
 })
